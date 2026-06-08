892400dd86698e92536d52132c908c1646ecbe11
Yuvraj Singh <yuvrajsingh43344@gmail.com>
2026-06-08 13:19:06 +0530

    [yuvraj]Add:BasicsProblems<all BasicsProblem Solved>

---
+++ core-java-practice/gcr-codebase/practice-problem/power.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class power {
+ public static void main(String[] args) {
+  Scanner sc=new Scanner(System.in);
+ 
+
+        System.out.print("Enter base: ");
+        double base = sc.nextDouble();
+
+        System.out.print("Enter exponent: ");
+        double exponent = sc.nextDouble();
+
+        double power = Math.pow(base, exponent);
+        System.out.println("Result = " + power);
+
+        sc.close();
+ }
+ 
+}
